f395c7edafa653f3fe9c5eac6d57fdc3fe519d23
Muhammad Nauman <mhmmd.nauman@gmail.com>
2022-04-11 00:07:01 +0500

    dsbc completed

--- cbc/transformations-brands.php
+++ cbc/transformations-brands.php
@@ -34,14 +34,21 @@ require '../vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 
-$spreadsheet1 = IOFactory::load("export_catalog_product_20220317_121244.xlsx");
+$spreadsheet1 = IOFactory::load("export_catalog_product_20220410_131200.xlsx");
 $sheetData1 = $spreadsheet1->getActiveSheet()->toArray(null, true, true, true);
+
+$spreadsheet = IOFactory::load("brands.xlsx");
+$sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+$brands=array();
+foreach($sheetData as $row){
+		if(!empty($row['A'])){
+			$brands[] = trim($row['A']);
+		}
+}
+
 $a=array();
 $b=array();
-$c=array();
-$mrsp=array();
-$names = array();
-$qty = array();
+
 
 
 
@@ -49,42 +56,120 @@ $qty = array();
 //print_r($sheetData);
 //echo"</pre>";
 //exit();
-outputCSV(array(
-													   array(
-														   'sku',
-														   'product_brand',
-														   
-														   
-						)));
+
 foreach($sheetData1 as $row){
 	
 		$name = trim($row['G']);
 		$sku = trim($row['A']);
-		//$ecr_page = trim($row['P']);
 		
 		
-				if(strpos($name,$brand)!== false){
-					
-							
-				outputCSV(array(
-													   array(
-														   trim($sku), // sku
-														   $brand,//store_view_code
-														   
-														   
-
-						)));
-				}else{
+		foreach($brands as $brand){
+				if(strpos($name,$brand)!== false && strpos($row['AU'],"product_brand")=== false){
+					$str = "product_brand=$brand";
+					if(!empty($row['AU'])){
+						$str = $row['AU'].",product_brand=$brand";
+					}
 					outputCSV(array(
-													   array(
-														   trim($sku), // sku
-														   "",//store_view_code
+														   array(
+															$row['A'], // sku
 														   
+														   $row['B'],
+														   $row['C'],
+														   $row['D'],
+														   $row['E'],
+														   $row['F'],
+														   $row['G'],// name
+														   $row['H'],
+														   $row['I'],
+														   $row['J'],
+														   $row['K'], // disable products product_online
+														   $row['L'],
+														   $row['M'],
 														   
+														   $row['N'],//price
+														   $row['O'],
+														   $row['P'],
+														   $row['Q'],
+														   $row['R'],// url_key
+														   $row['S'],// name
+														   $row['T'], // meta_keywords
+														   $row['U'], // name,
+														   $row['V'],
+														   $row['W'],
+														   $row['X'],
+														   $row['Y'],
+														   $row['Z'],
+														   $row['AA'], // 
+														   $row['AB'],
+														   $row['AC'],
+														   $row['AD'],
+														   $row['AE'],
+														   $row['AF'],
+														   $row['AG'],
+														   
+														   $row['AH'],
+														   $row['AI'],
+														   $row['AJ'], // msrp_price
+														   $row['AK'],
+														   $row['AL'],
+														   $row['AM'],
+														   $row['AN'],
+														   $row['AO'],
+														   $row['AP'],
+														   $row['AQ'],
+														   $row['AR'],
+														   $row['AS'],
+														   $row['AT'],
+														   $str,
+														   $row['AV'], // qty
+														   $row['AW'], // mark it out of stock
+														   $row['AX'],
+														   $row['AY'],
+														   $row['AZ'],
+														   $row['BA'],
+														   $row['BB'],
+														   $row['BC'],
+														   $row['BD'], // max_cart_qty
+														   $row['BE'],
+														   $row['BF'],
+														   $row['BG'], // use_config_notify_stock_qty
+														   $row['BH'],
+														   $row['BI'],
+														   $row['BJ'],
+														   $row['BK'], // use_config_qty_increments
+														   $row['BL'],
+														   $row['BM'],
+														   $row['BN'], // enable_qty_increments
+														   $row['BO'],
+														   $row['BP'], // website_id
+														   $row['BQ'],
+														   $row['BR'],
+														   $row['BS'],
+														   $row['BT'],
+														   $row['BU'],
+														   $row['BV'], // upsell_position
+														   $row['BW'],
+														   $row['BX'], // additional_image_labels
+														   $row['BY'],
+														   $row['BZ'],
+														   $row['CA'],
+														   $row['CB'],
+														   $row['CC'],
+														   $row['CD'],
+														   $row['CE'],
+														   $row['CF'],
+														   $row['CG'],
+														   $row['CH'],
+														   $row['CI'],
+														   $row['CJ'],
+														   $row['CK'],
+															   
+															   
+															   
 
-						)));
+							)));
 				}
-			
+		}
 			
 }
 exit;
